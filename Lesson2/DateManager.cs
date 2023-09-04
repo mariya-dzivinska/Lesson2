81c8b7a75b57a20133f5684cad70754697e0ad22
mariya.dzivinska@gmail.com <mariya.dzivinska@gmail.com>
2023-09-04 20:13:52 +0300

    Add project files.

---
+++ Lesson2/DateManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class DateManager
+    {
+        public DateTime Date { get; set; }
+
+        public DateTime GetCurrentDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lesson2;
+
+Console.WriteLine("Hello, World!");
+
+var dateManager = new DateManager();
+
+void NewFun()
+{
+     void NewFunc2()
+    {
+        string NewFunc3()
+        {
+            return "new";
+        }
+
+        var a = NewFunc3();
+
+    }
+}
+
+var currentTime = dateManager.GetCurrentDate();
+Console.WriteLine(currentTime);
